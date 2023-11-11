f08866e6bc3c78620aa107cbf7d1c89fed1ddbf1
Julian <julianjones663@gmail.com>
2023-11-11 17:14:30 +0000

    added leave party and some more error handling

--- game.py
+++ game.py
@@ -1,7 +1,4 @@
-
-
 class Game:
-
     def __init__(self, max_players: int, name: str, min_players: int = 2):
         self.player_constraints = (min_players, max_players)
         self.name = name
@@ -11,20 +8,19 @@ class Game:
 
         args:
             player(int): index of the player to get the data for
-        
+
         returns:
             JSON encoded data :D
         """
         raise NotImplementedError("Implement this please :DDD")
 
     def is_valid_playercount(self, players: int) -> bool:
-        """ Checks if the given number of players is allowed
+        """Checks if the given number of players is allowed
 
         args:
             players(int): the number of players that the game is trying to be initialised with
-        
+
         returns:
             is the player count valid
         """
         return self.player_constraints[0] <= players <= self.player_constraints[1]
-        
