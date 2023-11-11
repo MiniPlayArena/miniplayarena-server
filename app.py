b253f25a0f3f9204833d6217e0cdbefd7ccc584b
Julian <julianjones663@gmail.com>
2023-11-11 14:10:48 +0000

    Updated flask

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
-limiter = Limiter(get_remote_address, app=app, default=["5 per second"], storage_uri="memory://")
+limiter = Limiter(get_remote_address, app=app, default_limits=["5 per second"], storage_uri="memory://")
 pusher = pusher.Pusher(
     app_id = "1706778",
     key = "7ccf4bc1ee9c8d02d536",
@@ -14,7 +14,7 @@ pusher = pusher.Pusher(
 # pusher.trigger(u"test-uno", u"init", {u"users": [u"user-1", u"user-2"]})
 
 if __name__ == "__main__":
-    from endpoints.auth import auth_pusher
-    app.register_blueprint(auth_pusher)
+    from endpoints.auth import auth
+    app.register_blueprint(auth)
     
     app.run(port=696, host="0.0.0.0")
